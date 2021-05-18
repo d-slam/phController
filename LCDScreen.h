4ab84766b04e9cddbef95521685de39c105e3bfc
Dami <71833645+d-slam@users.noreply.github.com>
2021-05-18 21:10:30 +0200

    Add files via upload

--- LCDScreen.h
+++ LCDScreen.h
@@ -8,19 +8,149 @@
 
 #include "Arduino.h"
 
+#include <LiquidCrystal.h>
 
 
+//LCD_INIT============================================
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+class LCDScreen
+{
+public:
+  LCDScreen()  {    lcd.begin(16, 2);  }
 
+  void drawSplashscreen()
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("ph-Meter");
+    lcd.setCursor(0, 1);
+    lcd.print("><(((°>");
+    delay(1500);
+    lcd.clear();
+  }
 
+  void redrawLCD()
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("ph Ist");
+    lcd.setCursor(0, 1);
+    lcd.print(phLast);
 
+    switch (SYSstate)
+    {
+    case SYS_RUN:
+    {
+      lcd.setCursor(8, 1);
+      lcd.print(phSoll);
+      lcd.setCursor(12, 1);
+      lcd.print("+0.5");
 
+      switch (RUNstate)
+      {
+      case RUN_RED:
+        lcd.setCursor(8, 0);
+        lcd.print("Pumpt   ");
+        break;
 
+      case RUN_YELLOW:
+        lcd.setCursor(8, 0);
+        lcd.print("In Range");
+        break;
 
+      case RUN_GREEN:
+        lcd.setCursor(8, 0);
+        lcd.print("Halte");
+        break;
+      }
+      break;
+    }
+    case SYS_WAIT:
+      lcd.setCursor(8, 1);
+      lcd.print("<-Lauf<<");
+      lcd.setCursor(8, 0);
+      lcd.print("Warten..");
+      break;
 
+    case SYS_SET_SOLL:
+      lcd.setCursor(8, 1);
+      lcd.print(phSoll);
+      lcd.setCursor(8, 0);
+      lcd.print("Set Soll");
+      break;
 
+    case SYS_CAL:
+    {
+      switch (CALstate)
+      {
+      case CAL_START:
+        lcd.setCursor(8, 1);
+        lcd.print("Start?");
+        lcd.setCursor(8, 0);
+        lcd.print("pH Cal");
+        break;
 
+      case CAL_PH4:
+        lcd.setCursor(8, 1);
+        lcd.print("Set?");
+        lcd.setCursor(8, 0);
+        lcd.print("ph 4");
+        break;
 
+      case CAL_PH7:
+        lcd.setCursor(8, 1);
+        lcd.print("Set?");
+        lcd.setCursor(8, 0);
+        lcd.print("ph 7");
+        break;
 
+      case CAL_CONF:
+        lcd.setCursor(8, 1);
+        lcd.print("Save?");
+        lcd.setCursor(8, 0);
+        lcd.print("Enter New Cal");
+        break;
+
+      case CAL_OK:
+        lcd.setCursor(8, 1);
+        lcd.print("Save?");
+        lcd.setCursor(8, 0);
+        lcd.print("okokl");
+        break;
+      }
+      break;
+    }
+    }
+  }
+
+private:
+};
+
+//btnINIT============================================
+int lcd_key = 0;
+int adc_key_in = 0;
+#define btnRIGHT 0
+#define btnUP 1
+#define btnDOWN 2
+#define btnLEFT 3
+#define btnSELECT 4
+#define btnNONE 5
+int read_LCD_buttons()
+{
+  adc_key_in = analogRead(0); // read the value from the sensor
+  if (adc_key_in > 1000)
+    return btnNONE;
+  if (adc_key_in < 50)
+    return btnRIGHT;
+  if (adc_key_in < 250)
+    return btnUP;
+  if (adc_key_in < 450)
+    return btnDOWN;
+  if (adc_key_in < 650)
+    return btnLEFT;
+  if (adc_key_in < 850)
+    return btnSELECT;
+  return btnNONE; // when all others fail, return this...
+}
 
 #endif
--- phController.ino
+++ phController.ino
@@ -1,34 +1,7 @@
-#include <LiquidCrystal.h>
 
-//LCD_INIT============================================
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-int lcd_key = 0;
-int adc_key_in = 0;
-#define btnRIGHT 0
-#define btnUP 1
-#define btnDOWN 2
-#define btnLEFT 3
-#define btnSELECT 4
-#define btnNONE 5
-int read_LCD_buttons()
-{
-  adc_key_in = analogRead(0); // read the value from the sensor
-  if (adc_key_in > 1000)
-    return btnNONE;
-  if (adc_key_in < 50)
-    return btnRIGHT;
-  if (adc_key_in < 250)
-    return btnUP;
-  if (adc_key_in < 450)
-    return btnDOWN;
-  if (adc_key_in < 650)
-    return btnLEFT;
-  if (adc_key_in < 850)
-    return btnSELECT;
-  return btnNONE; // when all others fail, return this...
-}
 
-//GLOBALS===============================================
+
+//OVER!GLOBALS===============================================
 #define RX_PH A1
 #define MOTORGATE 2
 
@@ -60,6 +33,17 @@ typedef enum
 } statesRun;
 statesRun RUNstate = RUN_GREEN;
 
+float volt = 0.0;
+
+float phSoll = 5.5;
+float phSollThres = 0.5;
+float phIst, phLast = 0.0;
+
+
+#include "LCDScreen.h"
+
+//GLOBALS===============================================
+
 bool incFlag = false;
 bool decFlag = false;
 
@@ -68,17 +52,13 @@ bool btnPrellFlag = false;
 float calDelta = 2.19;
 float calOffset = 2.85;
 
-float volt = 0.0;
-
-float phSoll = 5.5;
-float phSollThres = 0.5;
-float phIst, phLast = 0.0;
-
 int nSmooth = 20;
 int incBuffer = 0;
 float vecBuffer = 0;
 
 int incStateCheck = 0;
+
+LCDScreen lcdScreen;
 
 //bool noPrell = true;
 
@@ -90,15 +70,9 @@ void setup()
   Serial.begin(9600);
   Serial.println("Hüü");
 
-  lcd.begin(16, 2); // <== WICHTIG!!
-  lcd.setCursor(0, 0);
-  lcd.print("ph-Meter");
-  lcd.setCursor(0, 1);
-  lcd.print("><(((°>");
-  delay(1500);
-  lcd.clear();
-
-  redrawLCD();
+  lcdScreen.drawSplashscreen();
+  lcdScreen.redrawLCD();
+  
 }
 
 //LOOP==========================================================
@@ -107,7 +81,7 @@ void loop()
   bufferPh();
 
   //if (incBuffer >= nSmooth - 1 || (noPrell == false && adc_key_in != 1023) )
-  redrawLCD();
+  lcdScreen.redrawLCD();
 
   incStateCheck++;
   if (SYSstate == SYS_RUN && incStateCheck >= 50)
@@ -116,99 +90,6 @@ void loop()
   checkButtons();
 }
 //MyMethodes==========================================================
-void redrawLCD()
-{
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("ph Ist");
-  lcd.setCursor(0, 1);
-  lcd.print(phLast);
-
-  switch (SYSstate)
-  {
-  case SYS_RUN:
-  {
-    lcd.setCursor(8, 1);
-    lcd.print(phSoll);
-    lcd.setCursor(12, 1);
-    lcd.print("+0.5");
-
-    switch (RUNstate)
-    {
-    case RUN_RED:
-      lcd.setCursor(8, 0);
-      lcd.print("Pumpt   ");
-      break;
-
-    case RUN_YELLOW:
-      lcd.setCursor(8, 0);
-      lcd.print("In Range");
-      break;
-
-    case RUN_GREEN:
-      lcd.setCursor(8, 0);
-      lcd.print("Halte");
-      break;
-    }
-    break;
-  }
-  case SYS_WAIT:
-    lcd.setCursor(8, 1);
-    lcd.print("<-Lauf<<");
-    lcd.setCursor(8, 0);
-    lcd.print("Warten..");
-    break;
-
-  case SYS_SET_SOLL:
-    lcd.setCursor(8, 1);
-    lcd.print(phSoll);
-    lcd.setCursor(8, 0);
-    lcd.print("Set Soll");
-    break;
-
-  case SYS_CAL:
-  {
-    switch (CALstate)
-    {
-    case CAL_START:
-      lcd.setCursor(8, 1);
-      lcd.print("Start?");
-      lcd.setCursor(8, 0);
-      lcd.print("pH Cal");
-      break;
-
-    case CAL_PH4:
-      lcd.setCursor(8, 1);
-      lcd.print("Set?");
-      lcd.setCursor(8, 0);
-      lcd.print("ph 4");
-      break;
-
-    case CAL_PH7:
-      lcd.setCursor(8, 1);
-      lcd.print("Set?");
-      lcd.setCursor(8, 0);
-      lcd.print("ph 7");
-      break;
-
-    case CAL_CONF:
-      lcd.setCursor(8, 1);
-      lcd.print("Save?");
-      lcd.setCursor(8, 0);
-      lcd.print("Enter New Cal");
-      break;
-
-    case CAL_OK:
-      lcd.setCursor(8, 1);
-      lcd.print("Save?");
-      lcd.setCursor(8, 0);
-      lcd.print("okokl");
-      break;
-    }
-    break;
-  }
-  }
-}
 
 void checkButtons()
 {
